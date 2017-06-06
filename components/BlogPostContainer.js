424e15f2e616c698b3dffa53c02b010453c8452e
Syndia <devteam@syndia.nl>
2017-06-06 17:31:45 +0200

    Testing step 7
    @todo multi layout, @see
    https://github.com/phenomic/phenomic/blob/master/packages/preset-react-a
    pp/docs/getting-started/7.md

--- components/BlogPostContainer.js
+++ components/BlogPostContainer.js
@@ -2,19 +2,25 @@ import React from 'react'
 import Head from "react-helmet"
 import { createContainer, query, BodyRenderer } from "@phenomic/preset-react-app/lib/client"
 
+// multi layouts @see https://github.com/phenomic/phenomic/blob/master/packages/preset-react-app/docs/getting-started/7.md @todo !
+
+import Layout from './Layout'
+
 const BlogPost = ({ page }) => (
-  <div>
-    {page.node && (
-      <article>
-        <Head>
-          <title>{ page.node.title }</title>
-          <meta name="description" content={ "" /* page.node.body.slice(0, 50)*/ } />
-        </Head>
-        <h1>{ page.node.title }</h1>
-        <BodyRenderer>{ page.node.body }</BodyRenderer>
-      </article>
-    )}
-  </div>
+  <Layout>
+    <div>
+      {page.node && (
+        <article>
+          <Head>
+            <title>{ page.node.title }</title>
+            <meta name="description" content={ "" /* page.node.body.slice(0, 50)*/ } />
+          </Head>
+          <h1>{ page.node.title }</h1>
+          <BodyRenderer>{ page.node.body }</BodyRenderer>
+        </article>
+      )}
+    </div>
+  </Layout>
 )
 
 const BlogPostContainer = createContainer(BlogPost, (props) => ({
--- components/HomeContainer.js
+++ components/HomeContainer.js
@@ -1,32 +1,35 @@
 import React from "react"
 import Head from "react-helmet"
 import { createContainer, query, BodyRenderer } from "@phenomic/preset-react-app/lib/client"
-
 import { Link } from "react-router"
 
+import Layout from './Layout'
+
 const Home =  ({ posts }) => (
-  <div>
-    <Head>
-      <title>Hello world</title>
-      <meta name="description" content="Everything is awaysome!" />
-    </Head>
-    <h1>Home</h1>
-    <ul>
-      { posts && posts.node && posts.node.list &&
-        posts.node.list.map((post) => (
-          <li key={post.id}>
-            <Link to={ `/blog/${ post.id }`}>{ post.title || post.id }</Link>
-          </li>
-        ))
-      }
-    </ul>
-    <p>
-      {
-        posts && posts.node && posts.node.hasNextPage &&
-        <Link to={ `/after/${ posts.node.next }`}>Older posts</Link>
-      }
-    </p>
-  </div>
+  <Layout>
+    <div>
+      <Head>
+        <title>Hello world</title>
+        <meta name="description" content="Everything is awaysome!" />
+      </Head>
+      <h1>Home</h1>
+      <ul>
+        { posts && posts.node && posts.node.list &&
+          posts.node.list.map((post) => (
+            <li key={post.id}>
+              <Link to={ `/blog/${ post.id }`}>{ post.title || post.id }</Link>
+            </li>
+          ))
+        }
+      </ul>
+      <p>
+        {
+          posts && posts.node && posts.node.hasNextPage &&
+          <Link to={ `/after/${ posts.node.next }`}>Older posts</Link>
+        }
+      </p>
+    </div>
+  </Layout>
 )
 
 const HomeContainer = createContainer(Home, (props) => ({
